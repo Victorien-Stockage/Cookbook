f6b9e7b7540d4e7f4b9496bfe2032654b59acff3
vclement <victorien.clement@univ-ubs.fr>
2025-10-19 17:33:24 +0200

    Banner

--- Cookbook.py
+++ Cookbook.py
@@ -3,18 +3,23 @@
 #|      Packages       |
 #=======================
 import streamlit as st
+from PIL import Image
 
- 
+#=======================
+#|        Setup        |
+#=======================
 with open('Styles.css') as f:
     css = f.read()
-
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 
+background = Image.open('Pictures\Main_page\Banner.png')
+st.image(background, width=1920)
 
-#=====================
-#|      Script       |
-#=====================
-st.title("Livre de recettes de Victorien 🍰")
+
+#=======================
+#|       Script        |
+#=======================
+st.markdown("# Livre de recettes de Victorien 🍰")
 st.markdown(
     "Cette application regroupe un ensemble de recettes qu'elles soient de cuisine 🍔, pâtisserie 🧁, chocolaterie 🍫, confiserie 🍬, ... Mais aussi des conseils et astuces quand à la réalisation de ces dernières ou bien le choix de couleurs/ingrédients/accords."
 )
