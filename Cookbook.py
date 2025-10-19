e42ff70ac7ea0d88d7e7efa766c1cb35afd66579
vclement <victorien.clement@univ-ubs.fr>
2025-10-19 16:06:42 +0200

    Initialisation de l'appli de livvre de recette

--- Cookbook.py
+++ Cookbook.py
@@ -1,1 +1,15 @@
 
+#=======================
+#|      Packages       |
+#=======================
+import streamlit as st 
+
+
+
+#=====================
+#|      Script       |
+#=====================
+st.title("Livre de recettes et conseils de Victorien")
+st.write(
+    "Cette application regroupe un ensemble de recettes qu'elles soient de cuisine, pâtisserie, chocolaterie, confiserie, ..., des conseils et astuces quand à la réalisation de ces dernières ou bien le choix de couleurs/ingrédients/accords. L'objectif de cette application est de rassembler les  différentes recettes que  j'ai pu trouver, tester, modifier au cours du temps afin de centraliser et de pouvoir les partager."
+)
