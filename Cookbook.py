ef0ea5aca3e67242f505514565464a65898f7bcb
vclement <victorien.clement@univ-ubs.fr>
2025-10-19 18:53:17 +0200

    MAJ

--- Cookbook.py
+++ Cookbook.py
@@ -32,9 +32,10 @@ st.markdown(
 )
 
 st.markdown(
-    "Cette application regroupe un ensemble de recettes qu'elles soient de cuisine 🍔, pâtisserie 🧁, chocolaterie 🍫, confiserie 🍬, ... Mais aussi des conseils et astuces quand à la réalisation de ces dernières ou bien le choix de couleurs/ingrédients/accords."
-)
-st.markdown(
-    "L'objectif de cette application est de rassembler les différentes recettes que j'ai pu trouver 🔎, tester 🧪, modifier 🥣 au cours du temps afin de pouvoir les centraliser et les partager."
+    """
+    Cette application regroupe un ensemble de recettes qu'elles soient de cuisine 🍔, pâtisserie 🧁, chocolaterie 🍫, confiserie 🍬, ... Mais aussi des conseils et astuces quand à la réalisation de ces dernières ou bien le choix de couleurs/ingrédients/accords.
+    
+    L'objectif de cette application est de rassembler les différentes recettes que j'ai pu trouver 🔎, tester 🧪, modifier 🥣 au cours du temps afin de pouvoir les centraliser et les partager.
+    """
 )
 
